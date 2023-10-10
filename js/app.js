af779de40ce088880f4e8d49d764964a0aaabb01
Raul Miranda <raulomiranda@hotmail.com>
2023-10-10 15:43:17 -0300

    Atualizar os itens presentes no carrinho

--- js/app.js
+++ js/app.js
@@ -1,37 +1,36 @@
-let carrinho = [];
 let valorTotal = [];
 
-function exibirTextoNaTela(tag, texto){
+function exibirTextoNaTela(tag, texto) {
     let campo = document.getElementById(tag);
     campo.innerHTML = texto;
 }
 
 function carrinhoZerado() {
     exibirTextoNaTela("valor-total", "")
-    exibirTextoNaTela("lista-produtos", "Carrinho vazio")
+    exibirTextoNaTela("lista-produtos", "")
 }
-//carrinhoZerado()
+carrinhoZerado()
 
 function adicionar() {
     let produto = document.querySelector("select").value;
     let quantidade = document.querySelector("input").value;
     let item = produto.split(" - R$");
-    let adicionarItem = carrinho.push(`${quantidade}x ${item[0]}`);
     let valor = item[1] * quantidade;
     let adicionaValor = valorTotal.push(valor);
-    //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce 
-    var somaValorTotal = valorTotal.reduce((accumulator,value) => accumulator + value,0);
-
-    console.log(carrinho, valorTotal);
-    exibirTextoNaTela(`valor-total`, `R$${somaValorTotal}`)
-    }
+    let somaValorTotal = valorTotal.reduce((accumulator,value) => accumulator + value,0);
     
-    //console.log(`${quantidade}x de ${produto}`);
+    
+    let resumoCarrinho = document.getElementById(`lista-produtos`);
+    resumoCarrinho.innerHTML = resumoCarrinho.innerHTML + (`lista-produtos`, ` <section class="carrinho__produtos__produto">
+    <span class="texto-azul"> ${quantidade}x </span> ${item[0]} <span class="texto-azul">R$${valor}</span>
+    </section>`);
+    exibirTextoNaTela(`valor-total`, `R$${somaValorTotal}`);
 
+}
 
 function limpar(){
-    carrinho = [];
     valorTotal = [];
     carrinhoZerado();
-    console.log(carrinho);
 }
+
+    //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce 
